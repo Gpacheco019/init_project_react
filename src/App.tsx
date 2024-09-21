6f4bdb5c2e07d17bc5f1090204ba425c581a7bf7
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2024-09-21 10:18:43 -0300

    feat: config-husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
 
-interface User {
-  id: number;
-  name: string;
-}
-
-interface UserService {
-  listUsers(): Promise<User[]>;
-}
-
-class UserService implements UserService {
-  async listUsers(): Promise<User[]> {
-    return fetch("/api/users").then((res) => res.json());
-  }
-}
+import './App.css';
+import reactLogo from './assets/react.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,17 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+          count is
+          {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
